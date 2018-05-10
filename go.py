d36ef4d341a7d3e1db3ef4dd450baa7fb1f9e2b6
xman <x@man.com>
2018-05-09 23:13:47 -0700

    1

--- go.py
+++ go.py
@@ -10,17 +10,6 @@ for i in range(1, ndf.shape[1]):
     ndf[:, i] = ndf[:, i - 1] * (df[str(i - 1)] + 1)
 
 INIT_BOARD = np.zeros(ndf.shape[1])
-# remove already added ones
-# print(df.index[380])
-# print(list(df.iloc[380, :].as_matrix()))
-# INIT_BOARD = np.copy(ndf[380])
-# print(INIT_BOARD)
-# df.drop(df.index[380])
-
-# d = df.as_matrix().astype(np.float32)
-# ndf = np.ones((d.shape[0], d.shape[1] + 1)).astype(np.float32)
-# for i in range(1, ndf.shape[1]):
-#     ndf[:, i] = ndf[:, i - 1] * (df[str(i - 1)] + 1)
 
 names = list(df.index)
 
